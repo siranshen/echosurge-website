7b0c3046b09a9e3f85d7bf27ed9b5094a38b3c0b
Siran Shen <10250825+siranshen@users.noreply.github.com>
2025-06-26 17:20:06 +0800

    Update analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -48,20 +48,23 @@ export default async function RootLayout({
   setRequestLocale(locale)
 
   const fontClassName = locale.startsWith('zh') ? `${notoSansSC.variable} font-sans` : `${hostGrotesk.variable} font-sans`
+  const isCN = process.env.NEXT_PUBLIC_IS_CN === 'true'
+  const gtagId = isCN ? 'G-4DGHG97VFD' : 'G-8EQNB4Z321'
+  const hmId = isCN ? '54385ebe505504cafad559dd840b349f' : '3c24963f8914978d1b900f66a8eef4e5'
 
   return (
     <html lang={locale}>
       <head>{/* 这里可以放 meta、title 等 */}</head>
       <body className={fontClassName}>
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
-        <Script src='https://hm.baidu.com/hm.js?54385ebe505504cafad559dd840b349f' strategy='afterInteractive' />
-        <Script src='https://www.googletagmanager.com/gtag/js?id=G-J2W6PVM2NK' strategy='afterInteractive' />
+        <Script src={`https://hm.baidu.com/hm.js?${hmId}`} strategy='afterInteractive' />
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${gtagId}`} strategy='afterInteractive' />
         <Script id='gtag-init' strategy='afterInteractive'>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-J2W6PVM2NK');
+            gtag('config', '${gtagId}');
           `}
         </Script>
       </body>
